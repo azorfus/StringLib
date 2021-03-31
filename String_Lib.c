cb5c543cf3a505fcd55a42bf547f9849aabbe5c6
azorfus <72391760+azorfus@users.noreply.github.com>
2021-03-31 10:23:19 +0530

    Add files via upload

---
+++ String_Lib.c
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "String_Lib.h"
+
+void String_init(String* str)
+{
+	str->Data = NULL;
+	str->Len = 0;
+}
+
+void String_charp(String* dest, const char* Source)
+{
+	dest->Len = strlen(Source);
+	dest->Data = malloc(dest->Len + 1);
+	strcpy(dest->Data, Source);
+}
+
+void String_alloc_size(String* str, size_t size)
+{
+	str->Data = malloc(size);
+	str->Len = size-1;
+}
+
+void String_realloc(String* str, size_t size)
+{
+	str->Data = realloc(str->Data, str->Len + size);
+}
+
+void String_append(String* str, const char* source)
+{
+	size_t source_len = strlen(source);
+	str->Data = realloc(str->Data, str->Len + source_len + 1);
+	str->Len += source_len;
+	strcat(str->Data, source);
+}
+
+void String_destroy(String* str)
+{
+	free(str->Data);
+	str->Data = NULL;
+	str->Len = 0;
+}
+
+void trynow(String* str)
+{
+	printf("%s, %i", str->Data, str->Len);
+}
---
+++ String_Lib.h
@@ -0,0 +1,21 @@
+﻿#ifndef STRING_LIB_H
+#define STRING_LIB_H
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct String
+{
+	char* Data;
+	size_t Len;
+} String;
+
+void String_init(String* str);
+void String_charp(String* dest, const char* Source);
+void String_alloc_size(String* str, size_t size);
+void String_realloc(String* str, size_t size);
+void String_append(String* str, const char* source);
+void String_destroy(String* str);
+void trynow(String* str);
+
+#endif // !STRING_LIB
